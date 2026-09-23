0434b5c943ff8c6f531dc6f7e8222308ebc8d30b
roshcode21 <rodo.raudales@gmail.com>
2026-09-23 16:36:38 -0600

    Bump HDM cache for V11 interaction system

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="hdm-v10-rebuild";
+const CACHE="hdm-v11-depth";
 const CORE=["./","./index.html","./unete.html","./styles.css","./app.js","./join.js","./three-scene.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
