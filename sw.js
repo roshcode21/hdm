9a92a021c4b454ffc80360f0df6e3c1493dc1d65
roshcode21 <rodo.raudales@gmail.com>
2026-09-23 14:46:48 -0600

    Add lightweight offline service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE="hdm-v5-1";
+const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET")return;
+  const url=new URL(req.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
