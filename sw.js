6e0220531a142e03b580779b24661b2efc0f9310
roshcode21 <rodo.raudales@gmail.com>
2026-09-23 16:20:24 -0600

    Bump HDM V10 service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="hdm-v9-experience";
-const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="hdm-v10-rebuild";
+const CORE=["./","./index.html","./unete.html","./styles.css","./app.js","./join.js","./three-scene.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{const req=event.request;if(req.method!=="GET")return;const url=new URL(req.url);if(url.origin!==location.origin)return;event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(c=>c||caches.match("./index.html"))))});
