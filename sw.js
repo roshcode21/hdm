9bf1b1d7040a838c560c40e3f7b2728e06a8e985
roshcode21 <rodo.raudales@gmail.com>
2026-09-23 15:26:14 -0600

    Use network-first cache during HDM prototype development

--- sw.js
+++ sw.js
@@ -1,13 +1,33 @@
-const CACHE="hdm-v7-1";
+const CACHE="hdm-v8-clean";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch",event=>{
-  const req=event.request;
-  if(req.method!=="GET")return;
-  const url=new URL(req.url);
+  const request=event.request;
+  if(request.method!=="GET")return;
+
+  const url=new URL(request.url);
   if(url.origin!==location.origin)return;
-  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{
-    const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;
-  }).catch(()=>caches.match("./index.html"))));
+
+  event.respondWith(
+    fetch(request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(request).then(cached=>cached||caches.match("./index.html")))
+  );
 });
