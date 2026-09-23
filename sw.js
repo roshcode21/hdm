8773ce82bd2e33add4a85b385a9aed0c3727d2e3
roshcode21 <rodo.raudales@gmail.com>
2026-09-23 15:08:30 -0600

    Bump HDM cache for v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="hdm-v6-1";
+const CACHE="hdm-v7-1";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
